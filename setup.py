db1406f2b720740a0cff98db8b0a0b5e02196533
Seongbin Ga <sebyga@kaist.ac.kr>
2021-07-14 17:36:33 +0900

    PIP updated

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
+from setuptools import find_packages
 from setuptools import setup
 
 setup(
     name = 'pyapep',
-    version = '0.0.3',
-    license = 'PNU_Seongbin_Ga',
+    version = '0.0.6',
+    #license = 'PNU_Seongbin_Ga',
     author = 'Seongbin Ga',
     author_email = 'sebyga@gmail.com',
     url = 'https://sebyga.github.io/HompyTest/',
     keywords = ['adsorption','PSA','process simulation'],
     description = 'This is adsorption process simulation package made by Seongbin Ga.',
-    #package = ['pyapep'],
+    #packages = ['pyapep'],
+    packages = find_packages(),
+    pytho_require='>=3.5'
     #install_requires = ['numpy','scipy']
 )
